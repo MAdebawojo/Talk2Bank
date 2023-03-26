b75f55841630218f9fdc0fcae7723c1611c33d4a
OlamoyegunAyomide <olamoyegunayomide16@gmail.com>
2023-03-26 14:25:54 +0100

    I tested the code and i was able to get all the FAQs and the responses

--- server/bankbot.py
+++ server/bankbot.py
@@ -1,12 +1,22 @@
 import openai
+from flask import Flask
+from FAQ import faq
 
-#open api key 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
-openai.api_key = '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'
+#open api key- put your own api key
+# always remember to remove your ai key before you push the code
+openai.api_key = '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'
 
+faq_string = ""
+
+for i, (question, answer) in enumerate(faq.items()):
+    faq_string += f"{i+1}. FAQ: {question}\nResponse: {answer}\n\n"
+
+print(faq_string)
 request = openai.ChatCompletion.create(model = "gpt-3.5-turbo",
-messages = [{"role":"system", "content":"Your name is Talk2bank. You are a helpful assistant that detects and communicates with any african language to the user."}, 
-{"role":"user","content":message}]) #message is a variable that stores any request made by the customers.
+messages = [{"role":"system", "content":"Your name is Talk2bank. You are a helpful assistant that detects and communicates with any african language to the user. In addition to your main role, you are to answer any FAQ questions with the appropriate answer from faq_string"}, 
+{"role":"user","content":messsage}]) #message is a variable that stores any request made by the customers.
 
+print(request['choices'][0]['message']['content'])
 @app.route('/json-example', methods=['POST'])
 def json_example():
     request_data = request.get_json()
